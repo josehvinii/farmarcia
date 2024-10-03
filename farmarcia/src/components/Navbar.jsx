0aea1f3b18def24c96e50c1a474cc7fa713d5b6e
josehvinii <jvictordauzaker@gmail.com>
2024-10-03 14:59:39 -0300

    atualizacao farmarcia

--- farmarcia/src/components/Navbar.jsx
+++ farmarcia/src/components/Navbar.jsx
@@ -4,14 +4,12 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='Navbar-container'>
-       <div class="logo">Farmárcia</div>
-        <ul class="nav-links">
-        <img src="./images/farmarcia-logo-pq.avif" alt=""  />
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Serviços</a></li>
-            <li><a href="#">Contato</a></li>
-        </ul>
+      <img className='logoPequena' src="./images/farmarcia-logo-pq.avif" alt=""  />
+      <h1>Farmárcia</h1>
+      <button className='Home'>Home</button>
+      <button className='Sobre'>Sobre</button>
+      <button className='Produtos'>Produtos</button>
+      <button className='Servicos'>Serviços</button>
     </div>
   )
 }
